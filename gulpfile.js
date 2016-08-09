2eddc4fb8587c2c4ddbe6683f7eb337619d2f92b
Lindsie Canton <lindsie87@gmail.com>
2016-08-09 16:42:43 -0400

    setting up gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+gulp.task('default', ['browser-sync','styles', 'watch']);
+
+gulp.task('watch', function(){
+	gulp.watch('./assets/styles/**/*.scss', ['styles']);
+	gulp.watch('./*.html', reload);
+});
+
+gulp.task('browser-sync', function(){
+  browserSync.init({
+    server: './'  
+  })
+});
+
+
+gulp.task('styles', function(){
+	return gulp.src('./assets/styles/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+		.pipe(concat('main.css'))
+		.pipe(gulp.dest('./assets/styles/'))
+		.pipe(reload({stream: true}));
+});
